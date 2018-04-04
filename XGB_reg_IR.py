1354e48feee1e6446dcf8b5d5d20251cda36f5c1
hori1537 <yuki.horie@cgco.co.jp>
2018-04-04 14:25:19 +0900

    Update XGB_reg_IR.py

--- XGB_reg_IR.py
+++ XGB_reg_IR.py
@@ -13,63 +13,55 @@ raw_input = pandas.read.csv(open('x.csv'))
 
 raw_input_train, raw_input_test = train_test_split(raw_input,test_size=0.1)
 
-
 [sampleID_train,glassthickness_train,parameter_train,haze_train,totaltr_train,Rs_train]=np.hsplit(raw_input_train,[1,2,99,100,101,102])
 
 [sampleID_test,glassthickness_test,parameter_test,haze_test,totaltr_test,Rs_test]=np.hsplit(raw_input_test,[1,2,99,100,101,102]
 
-
-# xgboostƒ‚ƒfƒ‹‚Ìì¬
+# xgboostãƒ¢ãƒ‡ãƒ«ã®ä½œæˆ
 reg1 = xgb.XGBRegressor()
 reg2 = xgb.XGBRegressor()
 reg3 = xgb.XGBRegressor()
 
-
-
-# ƒnƒCƒp[ƒpƒ‰ƒ[ƒ^’Tõ
+# ãƒã‚¤ãƒ‘ãƒ¼ãƒ‘ãƒ©ãƒ¡ãƒ¼ã‚¿æ¢ç´¢
 reg1_cv = GridSearchCV(reg1, {'max_depth': [2,4,6], 'n_estimators': [50,100,200]}, verbose=1)
 reg_cv.fit(parameter_train,haze_train)
 print reg1_cv.best_params_, reg1_cv.best_score_
 
-# ƒnƒCƒp[ƒpƒ‰ƒ[ƒ^’Tõ
+# ãƒã‚¤ãƒ‘ãƒ¼ãƒ‘ãƒ©ãƒ¡ãƒ¼ã‚¿æ¢ç´¢
 reg2_cv = GridSearchCV(reg2, {'max_depth': [2,4,6], 'n_estimators': [50,100,200]}, verbose=1)
 reg2_cv.fit(parameter_train,totaltr_train)
 print reg2_cv.best_params_, reg2_cv.best_score_
 
-# ƒnƒCƒp[ƒpƒ‰ƒ[ƒ^’Tõ
+# ãƒã‚¤ãƒ‘ãƒ¼ãƒ‘ãƒ©ãƒ¡ãƒ¼ã‚¿æ¢ç´¢
 reg3_cv = GridSearchCV(reg3, {'max_depth': [2,4,6], 'n_estimators': [50,100,200]}, verbose=1)
 reg3_cv.fit(parameter_train,Rs_train)
 print reg3_cv.best_params_, reg3_cv.best_score_
 
-
-
-# ‰ü‚ß‚ÄÅ“Kƒpƒ‰ƒ[ƒ^‚ÅŠwK
+# æ”¹ã‚ã¦æœ€é©ãƒ‘ãƒ©ãƒ¡ãƒ¼ã‚¿ã§å­¦ç¿’
 reg1 = xgb.XGBRegressor(**reg1_cv.best_params_)
 reg1.fit(parameter_train, haze_train)
 
-# ‰ü‚ß‚ÄÅ“Kƒpƒ‰ƒ[ƒ^‚ÅŠwK
+# æ”¹ã‚ã¦æœ€é©ãƒ‘ãƒ©ãƒ¡ãƒ¼ã‚¿ã§å­¦ç¿’
 reg2 = xgb.XGBRegressor(**reg2_cv.best_params_)
 reg2.fit(parameter_train,totaltr_train)
 
-# ‰ü‚ß‚ÄÅ“Kƒpƒ‰ƒ[ƒ^‚ÅŠwK
+# æ”¹ã‚ã¦æœ€é©ãƒ‘ãƒ©ãƒ¡ãƒ¼ã‚¿ã§å­¦ç¿’
 reg3 = xgb.XGBRegressor(**reg3_cv.best_params_)
 reg3.fit(parameter_train,Rs_train)
 
-
-
-# ŠwKƒ‚ƒfƒ‹‚Ì•Û‘¶A“Ç‚İ‚İ
+# å­¦ç¿’ãƒ¢ãƒ‡ãƒ«ã®ä¿å­˜ã€èª­ã¿è¾¼ã¿
 # import pickle
 # pickle.dump(reg, open("model.pkl", "wb"))
 # reg = pickle.load(open("model.pkl", "rb"))
 
-# ŠwKƒ‚ƒfƒ‹‚Ì•]‰¿
+# å­¦ç¿’ãƒ¢ãƒ‡ãƒ«ã®è©•ä¾¡
 pred1_train = reg1.predict(paramater_train)
 pred1_test = reg1.predict(parameter_test)
 print mean_squared_error(haze_train, pred1_train)
 print mean_squared_error(haze_test, pred1_test)
 
 
-# feature importance ‚Ìƒvƒƒbƒg
+# feature importance ã®ãƒ—ãƒ­ãƒƒãƒˆ
 import pandas as pd
 import matplotlib.pyplot as plt
 importances = pd.Series(reg.feature_importances_)
